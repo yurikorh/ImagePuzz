472ad314c758fde6856f72a005ee5f202cf1224f
SunnyPai <138643497+SunnyPai0413@users.noreply.github.com>
2024-05-15 13:13:51 +0800

    Create CanvasArea.vue

---
+++ src/CanvasArea.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="canvas-area">
+    <canvas id="myCanvas"></canvas>
+  </div>
+</template>
+
+<script>
+import { onMounted } from 'vue';
+
+export default {
+  name: 'CanvasArea',
+  props: ['canvas', 'initCanvas', 'addPictures', 'addText', 'configureElement', 'removeElement', 'exportCanvas'],
+  setup(props) {
+    onMounted(() => {
+      props.initCanvas();
+    });
+  }
+};
+</script>
+
+<style scoped>
+.canvas-area {
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  width: 100%;
+  height: auto;
+}
+</style>
